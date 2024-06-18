19ff6f1573e9b98d92c1d3915f0d3d4f2c24efbf
Ushasree Kamala <ushasree210@gmail.com>
2024-06-17 22:58:35 -0500

    All the issues are resolved and  both api and web project are working fine.

--- MagicVilla_Web/Services/BaseService.cs
+++ MagicVilla_Web/Services/BaseService.cs
@@ -21,27 +21,105 @@ namespace MagicVilla_Web.Services
             this.httpClient = httpClient;
         }
         //above http  client is used to call the API
+        //        public async Task<T> SendAsync<T>(APIRequest apiRequest)
+        //        {
+        //            try
+        //            {
+        //                //Create HttpClient:
+
+
+        //                var client = httpClient.CreateClient("MagicAPI");
+        //                //Create HttpRequestMessage
+        //                HttpRequestMessage message = new HttpRequestMessage();
+        //                message.Headers.Add("Accept", "application/json");
+        //                message.RequestUri = new Uri(apiRequest.Url);
+        //                //data will not be null in post/put http calls
+        //                //Set Request Content (if any)
+        //                if (apiRequest.Data != null)
+        //                {
+        //                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+        //                          Encoding.UTF8, "application/json");
+
+        //                }
+        //                //Set HTTP Method:
+        //                switch (apiRequest.ApiType)
+        //                {
+        //                    case SD.ApiType.POST:
+        //                        message.Method = HttpMethod.Post;
+        //                        break;
+        //                    case SD.ApiType.PUT:
+        //                        message.Method = HttpMethod.Put;
+        //                        break;
+        //                    case SD.ApiType.DELETE:
+        //                        message.Method = HttpMethod.Delete;
+        //                        break;
+        //                    default:
+        //                        message.Method = HttpMethod.Get;
+        //                        break;
+        //                }
+        //                HttpResponseMessage apiResponse = null;
+
+        //                if(!string.IsNullOrEmpty(apiRequest.Token))
+        //                {
+        //                    client.DefaultRequestHeaders.Authorization=new AuthenticationHeaderValue("Bearer",apiRequest.Token);    
+        //                }
+        //                //Send the Request and Get Response
+        //                apiResponse = await client.SendAsync(message);
+        //                //Handle the Response
+        //                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+        //                try
+        //                {
+        //                    APIResponse ApiResponse = JsonConvert.DeserializeObject<APIResponse>(apiContent);
+        //                    if (ApiResponse != null)
+        //                    {
+        //                        if (ApiResponse.StatusCode == System.Net.HttpStatusCode.BadRequest
+        //                            || ApiResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
+        //                        {
+        //                            ApiResponse.StatusCode = System.Net.HttpStatusCode.BadRequest;
+        //                            ApiResponse.IsSuccess = false;
+        //                            var res = JsonConvert.SerializeObject(ApiResponse);
+        //                            var returnObj = JsonConvert.DeserializeObject<T>(res);
+        //                            return returnObj;
+        //                        }
+        //                    }
+        //                }
+        //                catch (Exception ex)
+        //                {
+        //                    var exceptionResponse = JsonConvert.DeserializeObject<T>(apiContent);
+        //                    return exceptionResponse;
+        //                }
+        //                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+        //                return APIResponse;
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                var dto = new APIResponse
+        //                {
+        //                    ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+        //                    IsSuccess = false
+        //                };
+
+        //            var res = JsonConvert.SerializeObject(dto);
+        //            var APIResponse = JsonConvert.DeserializeObject<T>(res);
+        //            return APIResponse;
+        //            }
+
+        //        }
+        //    }
+        //}
         public async Task<T> SendAsync<T>(APIRequest apiRequest)
         {
             try
             {
-                //Create HttpClient:
-
-
                 var client = httpClient.CreateClient("MagicAPI");
-                //Create HttpRequestMessage
                 HttpRequestMessage message = new HttpRequestMessage();
                 message.Headers.Add("Accept", "application/json");
                 message.RequestUri = new Uri(apiRequest.Url);
-                //data will not be null in post/put http calls
-                //Set Request Content (if any)
                 if (apiRequest.Data != null)
                 {
                     message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
-                          Encoding.UTF8, "application/json");
-                    
+                        Encoding.UTF8, "application/json");
                 }
-                //Set HTTP Method:
                 switch (apiRequest.ApiType)
                 {
                     case SD.ApiType.POST:
@@ -56,51 +134,52 @@ namespace MagicVilla_Web.Services
                     default:
                         message.Method = HttpMethod.Get;
                         break;
+
                 }
+
                 HttpResponseMessage apiResponse = null;
 
-                if(!string.IsNullOrEmpty(apiRequest.Token))
+                if (!string.IsNullOrEmpty(apiRequest.Token))
                 {
-                    client.DefaultRequestHeaders.Authorization=new AuthenticationHeaderValue("Bearer",apiRequest.Token);    
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiRequest.Token);
                 }
-                //Send the Request and Get Response
+
                 apiResponse = await client.SendAsync(message);
-                //Handle the Response
+
                 var apiContent = await apiResponse.Content.ReadAsStringAsync();
                 try
                 {
-                     APIResponse ApiResponse =JsonConvert.DeserializeObject<APIResponse>(apiContent);
-                    if(ApiResponse.StatusCode==System.Net.HttpStatusCode.BadRequest
-                        || ApiResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    APIResponse ApiResponse = JsonConvert.DeserializeObject<APIResponse>(apiContent);
+                    if (ApiResponse != null && (apiResponse.StatusCode == System.Net.HttpStatusCode.BadRequest
+                        || apiResponse.StatusCode == System.Net.HttpStatusCode.NotFound))
                     {
-                        ApiResponse.StatusCode =System.Net.HttpStatusCode.BadRequest;
+                        ApiResponse.StatusCode = System.Net.HttpStatusCode.BadRequest;
                         ApiResponse.IsSuccess = false;
                         var res = JsonConvert.SerializeObject(ApiResponse);
                         var returnObj = JsonConvert.DeserializeObject<T>(res);
                         return returnObj;
                     }
                 }
-                catch (Exception ex)
+                catch (Exception e)
                 {
                     var exceptionResponse = JsonConvert.DeserializeObject<T>(apiContent);
                     return exceptionResponse;
                 }
                 var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
                 return APIResponse;
+
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
                 var dto = new APIResponse
                 {
-                    ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                    ErrorMessages = new List<string> { Convert.ToString(e.Message) },
                     IsSuccess = false
                 };
-                
-            var res = JsonConvert.SerializeObject(dto);
-            var APIResponse = JsonConvert.DeserializeObject<T>(res);
-            return APIResponse;
+                var res = JsonConvert.SerializeObject(dto);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+                return APIResponse;
             }
-                
         }
     }
 }
